5550f6e9d656c8b1f031f45f9063ffce57bb12cc
Kristopher Ong <ongkristopher@gmail.com>
2017-11-02 09:33:24 +0800

    Update index.php
    Edited indentations and fix some incorrect spellings.

--- ajaxDrop/index.php
+++ ajaxDrop/index.php
@@ -4,12 +4,11 @@
 	<title></title>
 </head>
 <body>
-	<!-- drop down -->
 	<select name="product_category" id="product_category">
 		<option value="">Select a Category</option>
 		<option value="Basketball Shoes">Basketball Shoes</option>
 		<option value="Running Shoes">Running Shoes</option>
-		<option vlaue="Nike Sportswear">Nike Sportswear</option>
+		<option value="Nike Sportswear">Nike Sportswear</option>
 	</select>
 
 	<div class="products">
@@ -26,27 +25,26 @@
 		$("#product_category").change(function(){
 
 			$("#product_image").html("");
-      		$("#product_name").html("");
-      		$("#product_price").html("");
+      			$("#product_name").html("");
+      			$("#product_price").html("");
 
 			var products_category = $(this).val();
+			
 			$.ajax({
-    			url: file_url,
-    			type: "post",
-    			data: {"action" : "getProductInfo",
+    				url: file_url,
+    				type: "post",
+    				data: {"action" : "getProductInfo",
     					"product_category" : products_category},
-    			success: function(data) {
-
-    				data = $.parseJSON(data);
+    				success: function(data) {
+    					data = $.parseJSON(data);
     				
-    				$.each(data,function(i){
-
-      					$("#product_image").append("<br>"+data[i].product_image);
-      					$("#product_name").append("<br>"+data[i].product_name);
-      					$("#product_price").append("<br>"+data[i].product_price);
-      				});
-    			},
-    			error: function(req, err){ console.log('my message ' + err +" "+req); }
+    					$.each(data,function(i){
+      						$("#product_image").append("<br>"+data[i].product_image);
+      						$("#product_name").append("<br>"+data[i].product_name);
+      						$("#product_price").append("<br>"+data[i].product_price);
+      					});
+    				},
+    				error: function(req, err){ console.log('my message ' + err +" "+req); }
   			});  
 		});
 	});
